cdfcbc91d320a3718c1314b0ff5e4808e20ed825
ruozhuliufeng <18852971173@63.com>
2022-07-22 14:23:53 +0800

    项目初始化

---
+++ src/main/java/cn/aixuxi/ledger/LedgerApplication.java
@@ -0,0 +1,13 @@
+package cn.aixuxi.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedgerApplication.class, args);
+    }
+
+}
