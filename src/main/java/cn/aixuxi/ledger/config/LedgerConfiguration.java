66fa45a2c05e7917156f0668d025fffecbb7ed23
ruozhuliufeng <18852971173@63.com>
2022-09-13 16:51:40 +0800

    新增fundebug报警，默认关闭

--- src/main/java/cn/aixuxi/ledger/config/LedgerConfiguration.java
+++ src/main/java/cn/aixuxi/ledger/config/LedgerConfiguration.java
@@ -4,6 +4,7 @@ import cn.aixuxi.ledger.properties.LedgerJwtProperties;
 import cn.aixuxi.ledger.properties.LedgerSmmsProperties;
 import cn.aixuxi.ledger.properties.LedgerWebDavProperties;
 import cn.aixuxi.ledger.properties.LedgerWxProperties;
+import com.fundebug.Fundebug;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,4 +16,15 @@ import org.springframework.context.annotation.Configuration;
         LedgerWebDavProperties.class})
 public class LedgerConfiguration {
 
+    /**
+     * 使用 fundebug 监控项目报警信息并提醒
+     */
+    @Bean
+    public Fundebug getBean(){
+        // 使用项目的apiKey
+        Fundebug fundebug = new Fundebug("your fundebug api key");
+        // 默认关闭
+        fundebug.setSilent(true);
+        return fundebug;
+    }
 }
